5fccc00e4610f5fd2db49a3bb86ae40ca2e8ad93
suyeshamatya <hisuyesh@hotmail.com>
2015-04-29 11:23:04 +0200

    commit sample collection breadcrumbs

---
+++ portlets/BCNetBiobank-portlet/docroot/WEB-INF/src/com/bcnet/portlet/samplecollection/SampleCollectionBreadcrumbsPortlet.java
@@ -0,0 +1,11 @@
+package com.bcnet.portlet.samplecollection;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class SampleCollectionBreadcrumbsPortlet
+ */
+public class SampleCollectionBreadcrumbsPortlet extends MVCPortlet {
+ 
+
+}
