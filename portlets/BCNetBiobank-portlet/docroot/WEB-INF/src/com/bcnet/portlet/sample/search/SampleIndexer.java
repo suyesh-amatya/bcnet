0642137e3da98635bcc11fbb984153c7abc7f59d
suyeshamatya <hisuyesh@hotmail.com>
2015-10-20 22:53:39 +0200

    doGetDocument method complete in sampleindexer

--- portlets/BCNetBiobank-portlet/docroot/WEB-INF/src/com/bcnet/portlet/sample/search/SampleIndexer.java
+++ portlets/BCNetBiobank-portlet/docroot/WEB-INF/src/com/bcnet/portlet/sample/search/SampleIndexer.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 import javax.portlet.PortletURL;
 
 import com.bcnet.portlet.biobank.model.Sample;
+import com.bcnet.portlet.biobank.service.BiobankGeneralInformationLocalServiceUtil;
 import com.bcnet.portlet.biobank.service.SampleCollectionLocalServiceUtil;
 import com.liferay.portal.kernel.search.BaseIndexer;
 import com.liferay.portal.kernel.search.Document;
@@ -51,8 +52,28 @@ public class SampleIndexer extends BaseIndexer{
 		Document document = getBaseModelDocument(PORTLET_ID, sample);
 		
 		document.addText("sampleCollectionName", SampleCollectionLocalServiceUtil.getSampleCollection(sample.getSampleCollectionDbId()).getName());
+		document.addText("biobankName", BiobankGeneralInformationLocalServiceUtil.getBiobankGeneralInformation(sample.getBiobankDbId()).getBiobankName());
+		document.addKeyword("materialType", sample.getMaterialType());
+		document.addKeyword("container", sample.getContainer());
+		document.addText("storageTemperature", sample.getStorageTemperature());
+		document.addDate("sampledTime", sample.getSampledTime());
+		document.addText("anatomicalPartOntology", sample.getAnatomicalPartOntology());
+		document.addKeyword("anatomicalPartOntologyVersion", sample.getAnatomicalPartOntologyVersion());
+		document.addKeyword("anatomicalPartOntologyCode", sample.getAnatomicalPartOntologyCode());
+		document.addText("anatomicalPartOntologyDescription", sample.getAnatomicalPartOntologyDescription());
+		document.addText("anatomicalPartFreeText", sample.getAnatomicalPartFreeText());
+		document.addKeyword("sex", sample.getSex());
+		document.addNumber("ageLow", sample.getAgeLow());
+		document.addNumber("ageHight", sample.getAgeHigh());
+		document.addText("ageUnit", sample.getAgeUnit());
+		document.addText("diseaseOntology", sample.getDiseaseOntology());
+		document.addKeyword("diseaseOntologyVersion", sample.getDiseaseOntologyVersion());
+		document.addKeyword("diseaseOntologyCode", sample.getDiseaseOntologyCode());
+		document.addText("diseaseOntologyDescription", sample.getDiseaseOntologyDescription());
+		document.addText("diseaseFreeText", sample.getDiseaseFreeText());
+		document.addKeyword("countryOfOrigin", sample.getCountryOfOrigin());
 		
-		return null;
+		return document;
 	}
 
 	@Override
